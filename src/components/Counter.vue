1655cb3b8c8462616c7b5bd108622cb3109842f6
Jiah Lee <jiahlee@Jiahs-MacBook-Pro.local>
2021-04-21 19:17:14 +0900

    auto counter

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,6 +1,7 @@
 <template>
 	<button @click="counter += 1">Add 1</button>
 	<p>The button's been clicked {{ counter }} times.</p>
+	<p>auto counter : {{ autoCounter }}</p>
 </template>
 
 <script>
@@ -8,7 +9,13 @@ export default {
 	data() {
 		return {
 			counter: 0,
+			autoCounter: 0,
 		};
+	},
+	mounted() {
+		setInterval(() => {
+			this.autoCounter++;
+		}, 1000);
 	},
 };
 </script>
