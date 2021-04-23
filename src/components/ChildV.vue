71fd55286e78a47f6b62c397208d96c39e0d3402
Jiah Lee <jiahlee@Jiahs-MacBook-Pro.local>
2021-04-23 14:17:49 +0900

    slot positioning

---
+++ src/components/ChildV.vue
@@ -0,0 +1,10 @@
+<template>
+	<header>
+		<slot name="header" />
+	</header>
+	<slot />
+	<p>++ text from child ++</p>
+	<footer>
+		<slot name="footer" />
+	</footer>
+</template>
--- src/components/Slots.vue
+++ src/components/Slots.vue
@@ -5,15 +5,28 @@
 		<button @click="clicked()">call parent method</button>
 		<button @click="hey">call child method</button>
 	</child>
+	<ChildV>
+		<template v-slot:header>
+			<h2>title from slot</h2>
+		</template>
+		<template v-slot:default>
+			<p>main content from slot</p>
+		</template>
+		<template v-slot:footer>
+			<p>footer from slot</p>
+		</template>
+	</ChildV>
 	<h3>- Slots -</h3>
 </template>
 
 <script>
 import Child from './Child';
+import ChildV from './ChildV';
 
 export default {
 	components: {
 		Child,
+		ChildV,
 	},
 	methods: {
 		clicked() {
